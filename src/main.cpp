571a86e13fbb09661e751dc97575bdd2fea2d1f5
lilliamc <lilliamc@usc.edu>
2022-12-13 14:52:52 -0800

    read input one char at a time

--- src/main.cpp
+++ src/main.cpp
@@ -7,17 +7,25 @@
 #include <stdio.h>
 #include <signal.h>
 
+struct termios oldt, newt;
+
+// exit gracefully
 void catch_interrupt(int signum) {
+    // reset the terminal input settings
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    // print out summary
     std::cout << std::endl << "Elapsed time:" << std::endl;
     exit(signum);
 }
 
+/* ALARM */
 class Alarm {
 public:
     bool low;
     bool medium;
     bool high;
 
+    // print output
     void output() {
         while(true) {
             if (medium) {
@@ -29,10 +37,12 @@ public:
         }
     }
 
+    // listen for input
     void input() {
         char user_input;
         while(true) {
-            std::cin >> user_input;
+            // std::cin >> user_input;
+            user_input = getchar();
             std::cout << "*" << std::flush;
         }
     }
@@ -49,13 +59,22 @@ public:
 
 int main()
 {
+    // save the terminal input settings
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+
+    // don't echo input, and allow chars one at a time
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+
+    // specify what to do on ctrl-c
     signal(SIGINT, catch_interrupt);
 
     Alarm *alarm = new Alarm();
-    std::thread output_thread = alarm->output_t();
+    // std::thread output_thread = alarm->output_t();
     std::thread input_thread = alarm->input_t();
 
-    output_thread.join();
+    // output_thread.join();
     input_thread.join();
     std::cout<<"Done"<<std::endl;
     return 0;
